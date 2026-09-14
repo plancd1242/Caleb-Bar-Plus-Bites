3ba3429ab50c2fe9dba2beee8e7f14c0b3e776e1
plancd1242 <calebdorfman1242@gmail.com>
2026-09-14 12:38:13 -0400

    Configured Vite development server to use port 1567

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
-  server: { proxy: { '/api': { target: 'http://127.0.0.1:8787', ws: true } } },
+  server: {
+    port: 1567,
+    strictPort: true,
+    proxy: { '/api': { target: 'http://127.0.0.1:8787', ws: true } },
+  },
 });
